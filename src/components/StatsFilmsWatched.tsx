7bba934a69f36b1da619e84c725a92d852d50d95
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-05-24 16:50:53 +0100

    adds functionality to the films watched line graph

--- src/components/StatsFilmsWatched.tsx
+++ src/components/StatsFilmsWatched.tsx
@@ -1,4 +1,6 @@
+import { getUserById } from "../utils/apiUtils";
 import { Line } from "react-chartjs-2";
+import moment from "moment";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,9 +11,25 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import "../src/App.css";
+import "../App.css";
+import { useEffect, useState } from "react";
 
 export default function StatsFilmsWatched() {
+  const [filmsWatched, setFilmsWatched] = useState([]);
+  const [dataRequest, setDataRequest] = useState(false);
+  useEffect(() => {
+    getUserById(5).then((user) => {
+      setFilmsWatched(user.user.data.user.films);
+      setDataRequest(true);
+    });
+  }, []);
+
+  console.log(filmsWatched);
+  const date = filmsWatched[0]["date_watched"];
+  const month = moment(date, "YYYY-MM-DDTHH:mm:ss.SSS[Z]").format("MMMM");
+
+  //loop through the user films, convert with moment and save to new array, add that to line graph
+
   ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -51,22 +69,24 @@ export default function StatsFilmsWatched() {
     datasets: [
       {
         label: "Films Watched",
-        data: [1, 2, 3],
+        data: [filmsWatched.length],
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
       {
         label: "Hours Watched",
-        data: [6, 7, 8],
+        data: [filmsWatched.length],
         borderColor: "rgb(53, 162, 235)",
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
     ],
   };
 
-  return (
+  return dataRequest ? (
     <div className="chart-container">
       <Line options={options} data={data} />
     </div>
+  ) : (
+    <h2>ooops</h2>
   );
 }
--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
 
 const client = axios.create({
-	baseURL: "https://be-film-stat-app.onrender.com/api/",
+  baseURL: "https://be-film-stat-app.onrender.com/api/",
 });
 
 export const getFilms = async () => {
-	const { data } = await client.get("films");
-	return data["films"];
+  const { data } = await client.get("films");
+  return data["films"];
+};
+
+export const getUserById = (user_id: number) => {
+  return axios
+    .get(`https://be-film-stat-app.onrender.com/api/users/${user_id}`)
+    .then((user) => {
+      return { user };
+    });
 };
