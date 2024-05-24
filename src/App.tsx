f056260dc2e90556e507bfd118b39d8d6090e405
ksskol <skolotakristina.com>
2024-05-24 02:46:39 +0100

    Added header and log in/ log out pages. On submit transfer to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Header from "./components/Header";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp"
+import Homepage from "./components/Homepage";
 
 function App() {
-
   return (
     <main>
-      helloo!
+      <Header />
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/login/home" element={<Homepage />} />
+      </Routes>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
