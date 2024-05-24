e2a51409220efb9b450c2011a2e0051f373518be
Kim Lee <kimberleylee1990@gmail.com>
2024-05-24 16:31:58 +0100

    Add empty userAccountPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 
-
 function App() {
   return (
     <div>
       <Homepage />
-        <Navbar />
+      <Navbar />
     </div>
   );
 }
