c6a2ecba79f818c228309e4d27731960fd0187e2
ksskol <skolotakristina.com>
2024-05-24 03:35:00 +0100

    Made nav bar work for the films History

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
 import LogIn from "./components/LogIn-SignUp/LogIn";
-import SignUp from "./components/LogIn-SignUp/SignUp"
+import SignUp from "./components/LogIn-SignUp/SignUp";
 import Homepage from "./components/Homepage";
+import FilmHistoryPage from "./components/FilmHistoryPage";
 
 function App() {
   return (
@@ -13,10 +14,10 @@ function App() {
         <Route path="/" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
-        <Route path="/login/home" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/history" element={<FilmHistoryPage />} />
       </Routes>
     </main>
-
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,18 @@ import { MdHome } from "react-icons/md";
 import { PiFilmReelFill } from "react-icons/pi";
 import { VscGraphLine } from "react-icons/vsc";
 import { FaRegUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import styles from "../css modules/Navbar.module.css";
 
 export default function Navbar() {
   return (
     <nav className={styles.icons}>
+      <Link to="/home">
       <MdHome className={styles.home} />
-      <PiFilmReelFill className={styles.film} />
+      </Link>
+      <Link to="/history">
+        <PiFilmReelFill className={styles.film} />
+      </Link>
       <VscGraphLine className={styles.graph} />
       <FaRegUserCircle className={styles.user} />
     </nav>
