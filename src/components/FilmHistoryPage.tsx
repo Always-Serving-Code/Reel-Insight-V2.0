607263237e3082aa24b9cd8fb52b22bc93dde381
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-05-27 11:57:40 +0100

    removed navbar from film history page

--- src/components/FilmHistoryPage.tsx
+++ src/components/FilmHistoryPage.tsx
@@ -1,9 +1,7 @@
-import Navbar from "./Navbar";
 export default function FilmHistoryPage() {
   return (
     <div>
       <div>Films History</div>
-      <Navbar />
     </div>
   );
 }
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,7 +1,6 @@
 import StatCardStyle from "../components/styling/StatCardStyle";
 import StatsFilmsWatched from "./StatsFilmsWatched";
 import FilmSearch from "./FilmSearch";
-import Navbar from "./Navbar";
 
 export default function Homepage() {
   return (
