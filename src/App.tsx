6fddde2a0e2b265f179d609f1abc7a4850dec9be
Kim Lee <kimberleylee1990@gmail.com>
2024-05-27 11:37:45 +0100

    Add userAccountPage and css module

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
 import LogIn from "./components/LogIn";
-import SignUp from "./components/SignUp"
+import SignUp from "./components/SignUp";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
+import UserAccountPage from "./components/UserAccountPage";
 
 function App() {
   return (
@@ -15,10 +16,10 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/login/home" element={<Homepage />} />
+        <Route path="/user" element={<UserAccountPage />} />
       </Routes>
       <Navbar />
     </main>
-
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,20 @@ import { PiFilmReelFill } from "react-icons/pi";
 import { VscGraphLine } from "react-icons/vsc";
 import { FaRegUserCircle } from "react-icons/fa";
 import styles from "../css modules/Navbar.module.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className={styles.icons}>
-      <MdHome className={styles.home} />
+      <Link to="/">
+        <MdHome className={styles.home} />
+      </Link>
       <PiFilmReelFill className={styles.film} />
       <VscGraphLine className={styles.graph} />
-      <FaRegUserCircle className={styles.user} />
+
+      <Link to="/user">
+        <FaRegUserCircle className={styles.user} />
+      </Link>
     </nav>
   );
 }
