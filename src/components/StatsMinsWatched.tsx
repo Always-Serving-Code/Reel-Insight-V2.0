0bd9572a2d77e8d4953f2e6b7202dfe965b63857
Kim Lee <kimberleylee1990@gmail.com>
2024-05-27 15:27:17 +0100

    Add line chart to StatsMinsWatched and added StatsFilmsWatched to StatsPage

--- src/components/StatsMinsWatched.tsx
+++ src/components/StatsMinsWatched.tsx
@@ -1,19 +1,17 @@
-interface StatsMinsWatchedProps {
-  films: [
-    {
-      _id?: number | string;
-      title: string;
-      directors: string[];
-      genres: string[];
-      release_year: number;
-      poster_url: string;
-      synopsis: string;
-      lead_actors: string[];
-      runtime: number;
-      __v?: number;
-    }
-  ];
-}
+import { StatsMinsWatchedProps, watchMonth } from "../interfaces";
+import { Line } from "react-chartjs-2";
+import moment from "moment";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import "../App.css";
 
 export default function StatsMinsWatched(props: StatsMinsWatchedProps) {
   const { films } = props;
@@ -22,9 +20,93 @@ export default function StatsMinsWatched(props: StatsMinsWatchedProps) {
     totalMinsWatched += film.runtime;
   }
 
-  return (
-    <div>
-      <p>Total Minutes Watched: {totalMinsWatched}</p>
-    </div>
+  const monthArr: Array<object> = [];
+  if (films.length) {
+    films.forEach((film) => {
+      const date = film.date_watched;
+      const month = moment(date, "YYYY-MM-DDTHH:mm:ss.SSS[Z]").format("MMMM");
+      monthArr.push({ month: month, runtime: film.runtime });
+    });
+  }
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Number of minutes watched",
+      },
+    },
+    maintainAspectRatio: true,
+    aspectRatio: 1,
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const minsData: number[] = [];
+  const runtime = films.map((film) => film.runtime);
+  console.log(runtime, "runtime?");
+
+  labels.forEach((month: string) => {
+    let count = 0;
+    monthArr.forEach((watchMonth: watchMonth) => {
+      console.log(watchMonth, "watchMonth");
+      if (month === watchMonth.month) {
+        count += watchMonth.runtime;
+      }
+    });
+    minsData.push(count);
+  });
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Number of minutes watched",
+        data: minsData,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return films.length ? (
+    <>
+      <div className="chart-container">
+        <Line options={options} data={data} />
+      </div>
+      <div>
+        <p>
+          You've watched a total of {totalMinsWatched} minutes of film this year
+        </p>
+      </div>
+    </>
+  ) : (
+    <p>Loading...</p>
   );
 }
