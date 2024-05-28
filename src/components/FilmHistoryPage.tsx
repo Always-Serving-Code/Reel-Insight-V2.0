1a6939f2f8085bd8964a28f7353b366aabefe2b6
Kim Lee <kimberleylee1990@gmail.com>
2024-05-28 12:33:04 +0100

    Add general loading states

--- src/components/FilmHistoryPage.tsx
+++ src/components/FilmHistoryPage.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { getFilmsByUserId } from "../utils/apiUtils";
 import { Film } from "../interfaces";
+import Loading from "./Loading";
 
 export default function FilmHistoryPage() {
   const [filmsById, setFilmsById] = useState<Film[]>([]);
+  const [isLoading] = useState(true);
 
   useEffect(() => {
     getFilmsByUserId(5).then((data) => {
@@ -11,12 +13,18 @@ export default function FilmHistoryPage() {
     });
   }, []);
 
-  return (
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div className="film-history-page">
       <ul className="film-list">
         {filmsById.map((film) => (
           <li key={film._id} className="film-item">
-            <img src={film.poster_url} className="film-poster" alt={film.title} />
+            <img
+              src={film.poster_url}
+              className="film-poster"
+              alt={film.title}
+            />
             <h2 className="film-title">{film.title}</h2>
           </li>
         ))}
