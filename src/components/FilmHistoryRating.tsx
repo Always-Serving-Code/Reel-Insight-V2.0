e8908e59dba07dfdc3edf621cbebf4a76bb0a2b9
Kim Lee <kimberleylee1990@gmail.com>
2024-05-29 16:45:55 +0100

    Update CSS and components

--- src/components/FilmHistoryRating.tsx
+++ src/components/FilmHistoryRating.tsx
@@ -1,23 +1,23 @@
 import { RatingProps } from "../interfaces";
 
 export default function FilmHistoryRating({ rating }: RatingProps) {
-	return (
-		<div>
-			{[1, 2, 3, 4, 5].map((star) => {
-				return (
-					<span
-						className="watched-star"
-						style={{
-							cursor: "pointer",
-							color: rating >= star ? "gold" : "grey",
-							fontSize: "35px",
-						}}
-					>
-						{" "}
-						★{" "}
-					</span>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map((star) => {
+        return (
+          <span
+            className="watched-star"
+            style={{
+              cursor: "pointer",
+              color: rating >= star ? "gold" : "grey",
+              fontSize: "15px",
+            }}
+          >
+            {" "}
+            ★{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
 }
